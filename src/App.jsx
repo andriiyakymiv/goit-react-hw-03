3aba3ebff9f6e00e6688ac3a9a64923535b2a685
andrii <a.iakumiv@gmail.com>
2025-04-22 08:59:37 +0300

    Added a component (ContactList) for the contact list and its style

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,31 @@ import contactsData from './data/contacts.json';
 import ContactForm from './components/ContactForm/ContactForm';
 
 import './App.css';
+import ContactList from './components/ContactList/ContactList';
 
 function App() {
-  // const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
   const [contacts, setContacts] = useLocalStorage('key-contacts', contactsData);
 
-  // const handleChange = e => {
-  //   setSearchValue(e.target.value.toLowerCase().trim());
-  // };
+  const handleChange = e => {
+    setSearchValue(e.target.value.toLowerCase().trim());
+  };
 
   const addContact = contact => {
     setContacts([...contacts, contact]);
   };
 
-  // const handleDelete = id => {
-  //   setContacts(contacts.filter(contact => contact.id !== id));
-  // };
+  const handleDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
 
-  // const filteredContacts = contacts.filter(({ name }) => name.toLowerCase().includes(searchValue));
+  const filteredContacts = contacts.filter(({ name }) => name.toLowerCase().includes(searchValue));
   
   return (
     <>
       <h1>Phonebook</h1>
       <ContactForm addContact={addContact} />
+      <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
     </>
   );
 };
